03c20fa866ecc6cad655857a8fd30c64920b9d83
lzoffoli-tg <lzoffoli@technogym.com>
2025-12-22 17:43:48 +0100

    added all

---
+++ test/test_balance.py
@@ -0,0 +1,147 @@
+"""jump analysis testing for SingleJump, JumpExercise, and DropJump"""
+
+import sys
+from datetime import datetime
+from os.path import abspath, dirname, join
+
+import pandas as pd
+from plotly.graph_objects import Figure
+
+# add project root to path like other tests do
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+# import classes under test
+from src.labanalysis.protocols.protocols import Participant
+from src.labanalysis.protocols.balancetests import PlankBalanceTest, UprightBalanceTest
+from src.labanalysis.protocols.normativedata import (
+    uprightbalance_normative_values,
+    plankbalance_normative_values,
+)
+
+participant = Participant(recordingdate=datetime.now())
+
+plank_path = join(
+    dirname(__file__),
+    "assets",
+    "balance_data",
+    "plank_occhi_aperti.tdf",
+)
+
+singleleg_path = join(
+    dirname(__file__),
+    "assets",
+    "balance_data",
+    "stabilità_monopodalico_dx_occhi_aperti.tdf",
+)
+
+bilateral_eyesopen_path = join(
+    dirname(__file__),
+    "assets",
+    "balance_data",
+    "stabilità_bipodalico_occhi_aperti.tdf",
+)
+
+bilateral_eyesshut_path = join(
+    dirname(__file__),
+    "assets",
+    "balance_data",
+    "stabilità_bipodalico_occhi_chiusi.tdf",
+)
+
+
+def test_upright_bilateral_eyesopen():
+
+    # generate the tests
+    test = UprightBalanceTest.from_files(
+        bilateral_eyesopen_path,
+        participant,
+        "open",
+        "left_frz",
+        "right_frz",
+        uprightbalance_normative_values,
+    )
+
+    # check results
+    results = test.results
+    assert hasattr(results, "summary")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "analytics")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "figures")
+    assert isinstance(results.figures, dict)
+    figures = list(results.figures.values())[0]
+    assert all([isinstance(f, Figure) for f in results.figures.values()])
+
+
+def test_upright_bilateral_eyesshut():
+
+    # generate the tests
+    test = UprightBalanceTest.from_files(
+        bilateral_eyesshut_path,
+        participant,
+        "closed",
+        "left_frz",
+        "right_frz",
+        uprightbalance_normative_values,
+    )
+
+    # check results
+    results = test.results
+    assert hasattr(results, "summary")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "analytics")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "figures")
+    assert isinstance(results.figures, dict)
+    figures = list(results.figures.values())[0]
+    assert all([isinstance(f, Figure) for f in results.figures.values()])
+
+
+def test_upright_monolateral():
+
+    # generate the tests
+    test = UprightBalanceTest.from_files(
+        singleleg_path,
+        participant,
+        "open",
+        None,
+        "right_frz",
+        uprightbalance_normative_values,
+    )
+
+    # check results
+    results = test.results
+    assert hasattr(results, "summary")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "analytics")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "figures")
+    assert isinstance(results.figures, dict)
+    figures = list(results.figures.values())[0]
+    assert all([isinstance(f, Figure) for f in results.figures.values()])
+
+
+def test_plank():
+
+    # generate the tests
+    test = PlankBalanceTest.from_files(
+        plank_path,
+        participant,
+        "open",
+        "l_pst_frz",
+        "r_pst_frz",
+        "l_ant_frz",
+        "r_ant_frz",
+        plankbalance_normative_values,
+    )
+
+    # check results
+    results = test.results
+    assert hasattr(results, "summary")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "analytics")
+    assert isinstance(results.summary, pd.DataFrame)
+    assert hasattr(results, "figures")
+    assert isinstance(results.figures, dict)
+    figures = list(results.figures.values())[0]
+    assert all([isinstance(f, Figure) for f in results.figures.values()])
